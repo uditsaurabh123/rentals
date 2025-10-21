c209d4133435c104a6ef08d37dce731d753bbc92
uditsaurabh123 <uditsaurabh747@gmail.com>
2025-10-21 09:00:24 +0000

    Add all chnages

---
+++ rentals/rentals/doctype/udit_company_drivers/test_udit_company_drivers.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, udit and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestUdit_Company_Drivers(FrappeTestCase):
+	pass
---
+++ rentals/rentals/doctype/udit_company_vehicle/udit_company_vehicle.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2025, udit and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Udit_Company_Vehicle(Document):
+	def before_save(self):
+		self.set_title()
+	
+	def set_title(self):
+		self.title = f"{self.make} - {self.model} - {self.year}"
