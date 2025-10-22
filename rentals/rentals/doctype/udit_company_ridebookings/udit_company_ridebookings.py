ca089a5c74ca361496df995354deb668e44b5805
uditsaurabh123 <uditsaurabh747@gmail.com>
2025-10-22 15:48:18 +0000

    please add latest changes for total amount function

--- rentals/rentals/doctype/udit_company_ridebookings/udit_company_ridebookings.py
+++ rentals/rentals/doctype/udit_company_ridebookings/udit_company_ridebookings.py
@@ -3,7 +3,14 @@
 
 # import frappe
 from frappe.model.document import Document
-
+import frappe
 
 class Udit_Company_RideBookings(Document):
-	pass
+    def validate(self):
+        self.update_total_amount()        
+     
+    def update_total_amount(self): 
+        total_amount=0
+        for route in ( self.items or [] ):
+            total_amount += route.distance_in_km * self.rate 
+            self.total_amount = total_amount
