1fcddb7ffb9e0a8d3e7dc8acd37c9d2a38c86114
uditsaurabh123 <uditsaurabh747@gmail.com>
2025-10-22 23:45:42 +0000

    add all changes

--- rentals/rentals/doctype/udit_company_drivers/udit_company_drivers.py
+++ rentals/rentals/doctype/udit_company_drivers/udit_company_drivers.py
@@ -2,8 +2,8 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class Udit_Company_Drivers(Document):
+class Udit_Company_Drivers(WebsiteGenerator):
 	pass
