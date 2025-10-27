1092aa608e7a90be183dbcf614380939ff16614e
uditsaurabh123 <uditsaurabh747@gmail.com>
2025-10-27 22:38:09 +0000

    add the rentals

--- rentals/rentals/doctype/udit_company_drivers/udit_company_drivers.py
+++ rentals/rentals/doctype/udit_company_drivers/udit_company_drivers.py
@@ -3,7 +3,11 @@
 
 # import frappe
 from frappe.website.website_generator import WebsiteGenerator
-
+import frappe # Import frappe for database access
 
 class Udit_Company_Drivers(WebsiteGenerator):
-	pass
+    # This is the standard method for adding context to a Web View DocType
+    pass
+
+# Note: You can also define other custom methods in this class
+# to be called from hooks or other parts of the system.
---
+++ rentals/rentals/web_form/cars/cars.py
@@ -0,0 +1,4 @@
+import frappe
+
+def get_context(context):
+	print("get context methoda called for a car web form")
